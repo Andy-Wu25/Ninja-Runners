8f76fa382e23d8635689b31f32917dbbeafc4e77
Andy Wu <andywu2513@gmail.com>
2023-08-14 17:14:49 +0100

    updated character class

--- character.py
+++ character.py
@@ -4,19 +4,14 @@ pygame.init()
 
 class Character(pygame.sprite.Sprite):
     def __init__(self, x, y, scale):
-        # Call the parent class (sprite) constructor
         pygame.sprite.Sprite.__init__(self)
 
-        # Instance variable
-        # Load image into pygame
         image = pygame.image.load("NEA_image/player/Idle/0.png")
-        # Scales the image down to a small image
         self.img = pygame.transform.scale(
             image, (int(image.get_width() * scale), int(image.get_height() * scale)))
-        # Create a rectangle
         self.rect = self.img.get_rect()
-        # Center of the rectangle will be set to the x and y values above
         self.rect.center = (x, y)
 
+    # Display function
     def draw(self):
-        screen.blit(self.img, self.rect)  # Displays it onto the screen
+        screen.blit(self.img, self.rect)
