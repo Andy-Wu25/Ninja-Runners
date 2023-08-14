3f06e16fae19999a45d2d3661ea56610f6655d38
Andy Wu <andywu2513@gmail.com>
2023-08-14 17:29:10 +0100

    New player controls

--- main.py
+++ main.py
@@ -1,33 +1,57 @@
 import pygame 
 import character
+from pygame.constants import DROPTEXT
 
 pygame.init()
-
-SCREEN_WIDTH = 1000  
-SCREEN_HEIGHT = 800  
+clock = pygame.time.Clock()
+FPS = 60  
 
 # Game window
+SCREEN_WIDTH = 1000  
+SCREEN_HEIGHT = 800  
 screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_HEIGHT))
-
-# Game window title
 pygame.display.set_caption("Shooter Game")
 
-player = character.Character(100, 200, 0.15)
+# Background
+BROWN = (165, 42, 42)
+def draw_background():
+    screen.fill(BROWN)
 
-# Starting position of character
-x = 100
-y = 200
+# Player and enemy instance
+player = character.Character(100, 200, 0.15)
+enemy = character.Character("enemy", 200, 200, 0.175, 3)
 
 scale = 0.15
 run = True  
-while run:  
-    player.draw()
+while run: 
+    clock.tick(FPS)  
+    draw_background()  
+    player.draw() 
+    enemy.draw()
+    player.move(move_left, move_right) 
 
     # Event handler
-    for event in pygame.event.get():  
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:  
             run = False  
 
-    pygame.display.update() 
+        # Key presses
+        if event.type == pygame.KEYDOWN:  
+            if event.key == pygame.K_a:  
+                move_left = True
+            if event.key == pygame.K_d:  
+                move_right = True
+
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+        # Key releases
+        if event.type == pygame.KEYUP:  
+            if event.key == pygame.K_a:
+                move_left = False
+            if event.key == pygame.K_d:
+                move_right = False
+
+    pygame.display.update()  
 
 pygame.quit()
