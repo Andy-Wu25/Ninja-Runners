2f53a9217d9f594340b51068e0f70708b67f8ac9
Andy Wu <andywu2513@gmail.com>
2023-08-16 22:22:24 +0100

    new character class

--- character.py
+++ character.py
@@ -1,25 +1,13 @@
-import pygame
-import main
-
-GRAVITY = 0.5
-move_left = False
-move_right = False
-
 # Character class
 class Character(pygame.sprite.Sprite):
     def __init__(self, character_type, x, y, scale, speed, number_weapon, health):
-        # Call the parent class (sprite) constructor
         pygame.sprite.Sprite.__init__(self)
         self.alive = True
-        # Determines the different character types
         self.character_type = character_type
-        # Empty list for animation images
         self.animation_list = []
         # Index of the animation list
         self.index = 0
-        # Gets the time in the beginning to see how long has passed, used to update the player animation
         self.animation_time = pygame.time.get_ticks()
-        # Shows the index of the animation_list (masterlist), which contains different lists of animation images
         self.current_action = 0
         self.jump = False
         self.velocity_y = 0
@@ -27,14 +15,11 @@ class Character(pygame.sprite.Sprite):
 
         # Idle animation list
         idle_list = []
-        # For loop to load the different animation images
         for i in range(10):
             image = pygame.image.load(
                 f"NEA_image/{self.character_type}/Idle/{i}.png")
-            # Scales the image down to a small image
             image = pygame.transform.scale(
                 image, (int(image.get_width() * scale), int(image.get_height() * scale)))
-            # Add the image into the idle list
             idle_list.append(image)
         self.animation_list.append(idle_list)  # Index 0 in animation_list
 
@@ -78,46 +63,44 @@ class Character(pygame.sprite.Sprite):
             death_list.append(image)
         self.animation_list.append(death_list)  # Index 4 in animation list
 
-        # Create self.img to be used in the draw method
-        # This image will be the first index of the animation list
-        # Depending on the current action, it will call the index of that particular action animation list
         self.img = self.animation_list[self.current_action][self.index]
-        # Create a rectangle
+
         self.rect = self.img.get_rect()
-        # Center of the rectangle will be set to the x and y values above
+
         self.rect.center = (x, y)
-        # Speed variable will determine the number of pixels that the character moves by
+
         self.speed = speed
-        self.direction = 1  # 1 = facing right, -1 = facing left
-        # Boolean variable to determine when to change and flip the direction of the player
+
+        self.direction = 1  
+
         self.flip = False
 
-        # Determines the number of weapons that the player has
-        self.number_weapon = number_weapon
-        # Determines the health of the character instances
-        self.health = health
-        self.max_health = self.health
+        self.throw_cooldown = 0
 
-        # Determines the number of weapons that the player has at the start of the game. Used to set the ammo back to thhis value when the player dies and the game restarts
-        #self.starting_weapon = number_weapon
+
+        self.number_weapon = number_weapon
+
+        self.health = health
+
+        self.maximum_health = self.health
 
     # Move method
+
     def move(self, move_left, move_right):
-        # Movement variables start at 0
-        dx = 0  # Change in x
-        dy = 0  # Change in y
+
+        dx = 0 
+        dy = 0
 
         # Movement variables
-        # Move left
         if move_left:
-            dx = -self.speed  # Moving left will decrease the x-coordinate
-            self.flip = True  # Changes the initial direction of facing right to facing left
-            self.direction = -1  # -1 represents facing left
+            dx = -self.speed  
+            self.flip = True 
+            self.direction = -1 
 
         # Move right
         if move_right:
-            dx = self.speed  # Moving right will increase the x-coordinate
-            self.flip = False  # The initial direction of facing right will remain the same
+            dx = self.speed  
+            self.flip = False  
             self.direction = 1  # 1 represents facing right
 
         # Jump
@@ -140,11 +123,17 @@ class Character(pygame.sprite.Sprite):
         self.rect.y += dy  # Rectangle's y-coordinate will increase by dy
 
     # Update method, handles all the animations and cooldowns
+
     def update(self):
         self.animation()  # Call the animation method
-        # self.alive()  # Call the alive method to load the death animation if the instance has died
+        # Call the alive method to load the death animation if the instance has died
+        self.check_alive()
+        # Update throw cooldown
+        if self.throw_cooldown > 0:
+            self.throw_cooldown -= 1
 
     # Animation method
+
     def animation(self):
         ANIMATION_TIMER = 50  # Animation timer to keep track of when to load the next image
         # Change the animation image depending on the current action of the player
@@ -158,10 +147,27 @@ class Character(pygame.sprite.Sprite):
             self.index += 1
             # Check the current index against the total number of images in the current action list
             # If current action is 0 (idle), it will check the length of the list in index 0 of the animation list
+
+            # Check if the index is greater than or equal to the total number of images in the current action list
+            # This means that it has reached the end of the action animation
             if self.index >= len(self.animation_list[self.current_action]):
-                self.index = 0
+                # Check if the current action is 4, which is the death animation
+                if self.current_action == 4:
+                    # Make the index remain at the length of the death animation list - 1, which will be the last index of the list
+                    self.index = len(
+                        self.animation_list[self.current_action]) - 1
+
+                # Check if  the current action value is 3, which is the throw animation
+                elif self.current_action == 3:
+                    self.index = 0
+
+                    global throw_completed
+                    throw_completed = True  # The throw animation has completed
+                else:
+                    self.index = 0  # Reset all other animation index back to 0 so that it repeats through the animation images again
 
     # Changing player action method
+
     def new_action(self, different_action):
         if different_action != self.current_action:  # Check if current action is equal to new action
             # Set the current action to different_action if they are not the same
@@ -173,10 +179,13 @@ class Character(pygame.sprite.Sprite):
             self.index = 0
 
     # Throw method
+
     def throw(self):
-        if player.number_weapon > 0:  # Check that the player has not run out of weapons to throw
+        # Check that the player has not run out of weapons to throw
+        if player.number_weapon > 0 and self.throw_cooldown == 0:
             # Create a instance of the Weapon class
-            weapon = Weapon(self.rect.centerx + (100 * self.direction),
+            self.throw_cooldown = 50
+            weapon = Weapon(self.rect.centerx + (90 * self.direction),
                             self.rect.centery + 30, self.direction)
         # Add the instance to the sprite group
             weapon_group.add(weapon)
@@ -184,17 +193,20 @@ class Character(pygame.sprite.Sprite):
             player.number_weapon -= 1
 
     # Check alive method
-    '''def alive(self):
+
+    def check_alive(self):
         if self.health <= 0:  # Check if the health of the instance is below or equal to 0
             self.health = 0  # Set health to 0 so it is not negative
             self.alive = False  # The instance has died due to the health being <= 0
             # Set the different_action value to 4, causing the animation to change to index 4 of animation_list, which is the death animation
-            self.new_action(4)'''
+            self.new_action(4)
 
     # Draw method
+
     def draw(self):
         screen.blit(pygame.transform.flip(
             self.img, self.flip, False), self.rect)
+        #pygame.draw.rect(screen, RED, self.rect, 1)
     
     # Player action animations
     def player_actions(self):
